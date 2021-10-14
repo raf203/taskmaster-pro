4ad94ad3609a5e192ca116f9786e66249822788d
Rafael Cardoso <rrafa203@gmail.com>
2021-10-14 11:06:42 -0700

    add warning color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+ 
+  // remove old classes
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+
+  else if (Math.abs(moment().diff(time,"days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -12,6 +32,9 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+  //check due date
+  auditTask(taskLi);
 
 
   // append to ul list on the page
@@ -110,11 +133,20 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker ({
+    minDate: 1,
+    onClose: function() {
+      //when the calendar us closed, force a "change" event on the "dateInput"
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 
   // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -271,4 +303,11 @@ $("#trash").droppable({
 
 });
 
-// Finished 5.3.5
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+
+
+// Added Check If the Date Is in the Past
+// }; 5.4.6
